248f62b4fde0b249b2156991349bb906979c120a
CaRoxas <carlotacarballolol@gmail.com>
2023-12-10 13:40:35 +0000

    Dia 10/12
    Hacer que desaparezcan los botones al darle a botonTotal

--- Assets/Scripts/Total.cs
+++ Assets/Scripts/Total.cs
@@ -7,6 +7,10 @@ public class Total : MonoBehaviour
 {
     public GameObject[] cositas;
     public TMP_Text calculo;
+    public GameObject BotonCamara;
+    public GameObject BotonCrear;
+    public GameObject BotonMundo;
+    bool On = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +24,25 @@ public class Total : MonoBehaviour
         cositas = GameObject.FindGameObjectsWithTag("Frutal");
         cositas = GameObject.FindGameObjectsWithTag("Granero");
         cositas = new GameObject[cositas.Length];
-        
 
     }
     public void Cuentas()
     {
-        calculo.text = cositas.ToString();
+        if (On == false)
+        {
+            On = true;
+            BotonCamara.SetActive(false);
+            BotonCrear.SetActive(false);
+            BotonMundo.SetActive(false);
+            calculo.text = cositas.ToString();
+        }
+        else
+        {
+            On = false;
+            BotonCamara.SetActive(true);
+            BotonCrear.SetActive(true);
+            BotonMundo.SetActive(true);
+        }
+
     }
 }
